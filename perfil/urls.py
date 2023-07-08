9bce1c3234a36703d444f1a840899075b7309053
Vinicius Araujo <vini.araujo.l.a@gmail.com>
2023-07-07 23:51:02 -0300

    Class 03

--- perfil/urls.py
+++ perfil/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('deletar_categoria/<int:id>', views.deletar_categoria, name="deletar_categoria"),
     path('cadastrar_categoria/', views.cadastrar_categoria, name="cadastrar_categoria"),
     path('update_categoria/<int:id>', views.update_categoria, name="update_categoria"),
+    path('dashboard/', views.dashboard, name="dashboard"),
 ]
--- perfil/views.py
+++ perfil/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
-from .utils import calcula_total
-from .models import Conta, Categoria
+from perfil.utils import calcula_total, calcula_equilibrio_financeiro
+from perfil.models import Conta, Categoria
 from django.contrib import messages
 from django.contrib.messages import constants
 import imghdr
 from django.core.exceptions import ValidationError
+from extrato.models import Valores
+from datetime import datetime
 #from django.http import HttpResponse
 #from django.db.models import Sum
 
@@ -12,7 +14,24 @@ def home(request):
     if request.method == "GET":
         contas = Conta.objects.all()
         saldo_total = calcula_total(contas, 'valor')
-        return render(request, 'home.html', {'contas': contas, 'saldo_total': saldo_total,})
+
+        valores = Valores.objects.filter(data__month=datetime.now().month)
+        entradas = valores.filter(tipo='E')
+        saidas = valores.filter(tipo='S')
+
+        total_entradas = calcula_total(entradas, 'valor')
+        total_saidas = calcula_total(saidas, 'valor')
+
+        #TODO 04 Make home view dynamic for 2 Cards in the right (Saldo total, Gerenciar contas)
+
+        percentual_gastos_essenciais, percentual_gastos_nao_essenciais = calcula_equilibrio_financeiro()
+
+        return render(request, 'home.html', {'contas': contas,
+                                              'saldo_total': saldo_total,
+                                              "total_entradas":total_entradas,
+                                              "total_saidas":total_saidas,
+                                              "percentual_gastos_essenciais":percentual_gastos_essenciais,
+                                              "percentual_gastos_nao_essenciais":percentual_gastos_nao_essenciais})
 
 def gerenciar(request):
     if request.method == "GET":
@@ -149,3 +168,13 @@ def deletar_categoria(request, id):
         
         messages.add_message(request, constants.SUCCESS, 'Category deleted!')
         return redirect('/perfil/gerenciar/')
+    
+def dashboard(request):
+    if request.method == "GET":
+        dados = {}
+        categorias = Categoria.objects.all()
+
+        for categoria in categorias:
+            dados[categoria.categoria] = calcula_total(Valores.objects.filter(categoria=categoria),'valor') 
+
+        return render(request, 'dashboard.html', {'labels': list(dados.keys()), 'values': list(dados.values())})
